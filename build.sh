f375ded94031d14884dda913f4c76695464fd54c
Jose F. Morales <jfmcjf@gmail.com>
2016-11-21 12:57:39 +0100

    Install curl (needed by 'ciao get')

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk --update add git make tar
+apk --update add git make tar curl
 git clone https://github.com/ciao-lang/ciao
 rm /var/cache/apk/*
 export SHELL=/bin/sh # (otherwise build fails)
