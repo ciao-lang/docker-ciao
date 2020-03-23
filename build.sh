bb1cb93eb5307ecfcf610d62a8681942257bfca7
Jose F. Morales <jfmcjf@gmail.com>
2020-03-23 14:23:32 +0100

    undo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apk --update add git make tar curl
-#git clone https://github.com/ciao-lang/ciao
+git clone https://github.com/ciao-lang/ciao
 rm /var/cache/apk/*
 export SHELL=/bin/sh # (otherwise build fails)
 cd ciao
